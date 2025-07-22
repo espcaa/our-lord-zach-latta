d6041b125ee30444eb697d18e0a4f299e111bab3
Edna <88869424+Ednaordinary@users.noreply.github.com>
2025-07-22 03:29:26 -0600

    updating list maker

--- update-list.sh
+++ update-list.sh
@@ -3,7 +3,7 @@
 cd pics || exit 1
 printf "updating file list...\n"
 rm -f list.txt
-find . -type f ! -name "file_list.txt" | sed 's|^\./||' > list.txt
+find . -type f ! -name "list.txt" | sed 's|^\./||' > list.txt
 if [ $? -ne 0 ]; then
     printf "Error: Failed to update file list.\n"
     exit 1
